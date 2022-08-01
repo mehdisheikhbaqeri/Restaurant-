1e80dcbb1febcb73eee65f7a5b6d49e41db12d53
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-08-02 01:04:56 +0430

    debugging media query

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    navbar.classList.toggle("active");
-  menu.classList.toggle("fa-time");
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
 };
 
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
