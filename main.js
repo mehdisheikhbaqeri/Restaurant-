442e2ee88130e066d22762cb22d1fe594823e118
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-08-02 14:17:11 +0430

    add home section

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
+let logo = document.querySelector(".logo");
+let header = document.querySelector("header");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
@@ -10,3 +12,19 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
 };
+
+// dark navbar scroll
+
+document.addEventListener("scroll", function () {
+  console.log(document.documentElement.scrollTop);
+
+  if (document.documentElement.scrollTop > 0) {
+    logo.style.height = "20px";
+    header.classList.add("bg-black");
+    header.classList.add("txt-white");
+  } else {
+    logo.style.height = "35px";
+    header.classList.remove("bg-black");
+    header.classList.remove("txt-white");
+  }
+});
